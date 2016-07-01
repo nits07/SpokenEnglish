8c794d761ee901fac1dd6b54fea448126da6521b
nits07 <nsershiya@gmail.com>
2016-07-01 17:35:30 +0530

    Text to Speech and Speech recognition APIs implemented

--- app/src/main/java/com/spokenenglish/Home.java
+++ app/src/main/java/com/spokenenglish/Home.java
@@ -1,23 +1,120 @@
 package com.spokenenglish;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Home extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class Home extends Activity {
+    //Text to Speech
+    TextToSpeech textToSpeech;
+    EditText readText;
+    Button readButton;
+
+    //Speech to Text
+    private TextView writeText;
+    private Button speakButton;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        //Text to Speech
+        readText=(EditText) findViewById(R.id.readText);
+        readButton=(Button) findViewById(R.id.readButton);
+
+        textToSpeech=new TextToSpeech(getApplicationContext(),new TextToSpeech.OnInitListener(){
+            @Override
+            public void onInit(int status){
+                if(status!=TextToSpeech.ERROR)
+                    textToSpeech.setLanguage(new Locale("en","IN"));
+            }
+        });
+
+        readButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String toSpeak=readText.getText().toString();
+                Toast.makeText(getApplicationContext(),toSpeak,Toast.LENGTH_SHORT).show();
+                textToSpeech.speak(toSpeak,TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
+
+
+        //Speech to Text
+        writeText=(TextView) findViewById(R.id.writeText);
+        speakButton=(Button) findViewById(R.id.speakButton);
+
+        speakButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                promptSpeechInput();
+            }
+        });
     }
+
+    //Text to Speech
+    public void onPause(){
+        if(textToSpeech!=null){
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onPause();
+    }
+
+    //////////////Speech to Text/////////////////
+    private void promptSpeechInput(){
+        Intent intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+
+        try{
+            startActivityForResult(intent,REQ_CODE_SPEECH_INPUT);
+        }catch(ActivityNotFoundException e){
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Receiving Speech Input
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        switch (requestCode){
+            case REQ_CODE_SPEECH_INPUT:{
+                if(resultCode==RESULT_OK && null != data){
+                    ArrayList<String> result =
+                            data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    writeText.setText(result.get(0));
+                }
+                break;
+            }
+        }
+    }
+
+    //Menu and other
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
